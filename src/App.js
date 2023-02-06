b90dbede22d20e6567dfcd2c346b1809093009f1
drigoalexander <drigosihombinga@gmail.com>
2023-02-06 23:09:56 +0700

    test gravel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import React, { useState, useEffect } from "react";
+import { Link, Route, BrowserRouter } from "react-router-dom";
+import axios from "axios";
 
-function App() {
+const PokemonDetail = ({ match, location }) => {
+  const [pokemon, setPokemon] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${match.params.pokemonId}`)
+      .then((res) => {
+        setPokemon(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{pokemon.name}</h1>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => {
+        setPokemon(res.data.results);
+        console.log(res.data.results);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <ul className="flex items-center justify-center w-full flex-col gap-10 py-10">
+        {pokemon.map((item, index) => (
+          <Link
+            to={`/pokemon/${item.name}`}
+            className="text-center block text-blue-500"
+          >
+            <li
+              key={item.name}
+              className="flex flex-col gap-4 cursor-pointer border-[1px] rounded-xl p-6 shadow-sm hover:scale-105 duration-300 ease-in-out"
+            >
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                  index + 1
+                }.png`}
+                alt={item.name}
+              />
+              <p>{item.id}</p>
+
+              {item.name}
+            </li>{" "}
+          </Link>
+        ))}
+      </ul>
+    </BrowserRouter>
+  );
+};
 
 export default App;
